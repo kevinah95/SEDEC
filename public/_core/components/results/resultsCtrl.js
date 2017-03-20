48620180d117c86c4c3009ee816f2ef1ffbddae4
Kevin Hernandez Rostran <kevinah95@gmail.com>
2017-03-20 06:51:18 +0100

    SAVEPOINT

--- public/_core/components/results/resultsCtrl.js
+++ public/_core/components/results/resultsCtrl.js
@@ -1,9 +1,11 @@
 app.controller('resultsCtrl', function($scope, $timeout, $location) {
+    $scope.init = function() {
+        $scope.results = {}
+    }
     $scope.$watch("results", function(newVal) {
         if (newVal != null && newVal.length > 0) {
             console.log(newVal);
         }
-
-        console.log($scope.results);
+        console.log(newVal);
     });
 });
