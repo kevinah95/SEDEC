4c7f4d71588ce22968de073ec40ed90d4a3e463e
Kevin Hernandez Rostran <kevinah95@gmail.com>
2017-03-20 05:31:48 +0100

    Login

--- public/_core/directives/navbar/navbarCtrl.js
+++ public/_core/directives/navbar/navbarCtrl.js
@@ -3,12 +3,10 @@ app.controller('navbarCtrl', function($scope, $timeout, $location, $route) {
 
     $scope.home = function() {
         $location.path('/home').replace();
-        $scope.$apply();
     };
     $scope.profile = function() {
         $location.path('/profile');
         /*history.go(0);*/
-        $scope.$apply();
     };
 
     $(function() {
