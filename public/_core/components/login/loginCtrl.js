8cb52498b1be8f999f43113dba2ed9ffa0bfa4b1
Jasson Moya <jasc996@gmail.com>
2017-03-19 23:51:04 -0600

    Swag to Login

--- public/_core/components/login/loginCtrl.js
+++ public/_core/components/login/loginCtrl.js
@@ -3,14 +3,6 @@ app.controller('loginCtrl', function($scope, $http, $timeout, $location) {
     //When login fails, this becomes true 
     $scope.NotRegistered = false;
 
-    //Login Function
-    $scope.login = function() {
-        var $form = $('.form'),
-            allFields = $form.form('get values');
-        $form.form('validate form'); //Check form
-        //You can put here the database part, Kevin, if the result from the query is false (I mean, the guy is not registered in SEDEC, so you must change the boolean NotRegistered and set it in True) :D
-    };
-
     $.fn.api.settings.api = {
         'login': 'http://localhost:8080/api/users/login',
     };
@@ -33,7 +25,10 @@ app.controller('loginCtrl', function($scope, $http, $timeout, $location) {
             data: $(this).serialize(),
             onResponse: function(response) {
                 if (response.result == "invalid") {
-                    alert(response.result);
+                    $scope.NotRegistered = true;
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
                     return;
                 } else {
                     $location.path('/home').replace()
